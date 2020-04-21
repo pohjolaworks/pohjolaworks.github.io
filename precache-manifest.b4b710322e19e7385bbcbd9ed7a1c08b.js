fbaba54ac3c532bf0033b93da8788b62c9994a3f
Erkki Halinen <erkki-eemeli.halinen@cgi.com>
2020-04-21 20:30:01 +0300

    Initial commit

---
+++ precache-manifest.b4b710322e19e7385bbcbd9ed7a1c08b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc47af73b0583dd657c693b40af45152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2a4a521b5b5ca007e3",
+    "url": "/static/css/main.93eba4ee.chunk.css"
+  },
+  {
+    "revision": "74e788ec72f57d88ed45",
+    "url": "/static/js/2.c3c7681b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3c7681b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2a4a521b5b5ca007e3",
+    "url": "/static/js/main.29c881f5.chunk.js"
+  },
+  {
+    "revision": "164b342dfdf2290c6785",
+    "url": "/static/js/runtime-main.a5de39de.js"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/ig_logo_raster.0e84d592.png"
+  },
+  {
+    "revision": "8889964cb37e8d580ba3dff4087bae86",
+    "url": "/static/media/pw_logo.8889964c.svg"
+  }
+]);
